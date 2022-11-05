71fdbfd792b36ae05dbffff379bb94b714dd186d
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-11-05 15:47:58 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in) ;
+
+        System.out.print("n1 Sayısını Giriniz: ");
+        int n1 = input.nextInt();
+
+        System.out.print("n2 Sayısını Giriniz: ");
+        int n2 = input.nextInt();
+
+        int ebob = 0 ;
+
+        if (n1<n2) {
+            for (int i = 1 ; i<=n1 ; i++) {
+
+                if ((n1 % i ==0) && (n2 % i == 0)) {
+                    ebob = i ;
+                }
+            }
+            System.out.println("İki Sayının EBOBU: " + ebob);
+
+            int ekok = (n1*n2) / ebob ;
+
+            System.out.println("İki Sayının EKOKU: " + ekok);
+
+        } else if (n2<n1) {
+            for (int i = 1 ; i<=n2 ; i++) {
+
+                if ((n1 % i ==0) && (n2 % i == 0)) {
+                    ebob = i ;
+                }
+            }
+            System.out.println("İki Sayının EBOBU: " + ebob);
+
+            int ekok = (n1*n2) / ebob ;
+
+            System.out.println("İki Sayının EKOKU: " + ekok);
+        }
     }
 }
